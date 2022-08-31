a17d6ba114190bc990771d571502961cc1496d36
Md.Shaikot Ikbal Hridoy <33354982+hridoy-software-developer@users.noreply.github.com>
2022-08-31 18:04:12 +0600

    update script

--- vanilla-spa/router.js
+++ vanilla-spa/router.js
@@ -1,36 +1,56 @@
-const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
-};
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+    const base_url = "http://localhost:3000";
+}else{
+    var base_url = "https://hridoy-software-developer.github.io/portfolio/vanilla-spa";
+}
 
 const routes = {
     404: "pages/404.html",
-    "/": "pages/index.html",
-    "/about": "pages/about.html",
-    "/lorem": "pages/lorem.html",
+    "": "pages/index.html",
+    "about": "pages/about.html",
+    "lorem": "pages/lorem.html",
 };
 
-const handleLocation = async () => {
-    if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
-        var base_url = "http://localhost:3000";
+const routeNavigator = (route) => {
+    if(route == ''){
+        window.history.pushState({}, "", base_url);
     }else{
-        var base_url = "https://hridoy-software-developer.github.io/portfolio/vanilla-spa/";
+        window.history.pushState({}, "", route);
     }
-    
-    // const path = window.location.pathname;
-    var path = location.href.match(/([^\/]*)\/*$/)[1];
-    console.log("base_url : "+base_url);
-    console.log("href : " + path);
+    window.route = route;
+    pageRender();
+};
 
-    
-    const route = routes[path] || routes[404];
+const pageRender = async () => {
+    const route = routes[window.route] || routes[404];
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("main-page").innerHTML = html;
 };
 
-window.onpopstate = handleLocation;
-window.route = route;
+window.route = "";
+pageRender('');
 
-handleLocation();
+
+
+// const handleLocation = async () => {
+//     if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+//         var base_url = "http://localhost:3000";
+//     }else{
+//         var base_url = "https://hridoy-software-developer.github.io/portfolio/vanilla-spa/";
+//     }
+    
+//     // const path = window.location.pathname;
+//     var path = location.href.match(/([^\/]*)\/*$/)[1];
+//     console.log("base_url : "+base_url);
+//     console.log("href : " + path);
+
+
+//     const route = routes[path] || routes[404];
+//     const html = await fetch(route).then((data) => data.text());
+//     document.getElementById("main-page").innerHTML = html;
+// };
+
+// window.onpopstate = handleLocation;
+// window.route = route;
+
+// handleLocation();
