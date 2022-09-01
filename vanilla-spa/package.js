2aa98ec1ab6fbd4e065bbd0f34bc4b0ed8115bf5
Md.Shaikot Ikbal Hridoy <33354982+hridoy-software-developer@users.noreply.github.com>
2022-09-01 11:24:52 +0600

    update vanilla script

---
+++ vanilla-spa/package.js
@@ -0,0 +1,18 @@
+{
+  "name": "navigo-example",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.html",
+  "scripts": {
+    "start": "parcel index.html --open",
+    "build": "parcel build index.html"
+  },
+  "dependencies": {
+    "navigo": "8.8.12"
+  },
+  "devDependencies": {
+    "@babel/core": "7.2.0",
+    "parcel-bundler": "^1.6.1"
+  },
+  "keywords": []
+}
