3a739354f95bdc9322eee54a6ffea84cbf7c0827
Md.Shaikot Ikbal Hridoy <33354982+hridoy-software-developer@users.noreply.github.com>
2022-09-01 15:40:27 +0600

    update issue

---
+++ vanilla-spa/src/index.js
@@ -0,0 +1,15 @@
+import Navigo from "navigo";
+
+document.getElementById(
+  "app"
+).innerHTML = `<a href="/user/xxx/save?foo=bar#anchor-here" data-navigo>click me</a>`;
+
+const router = new Navigo("/");
+
+router.on("/user/:id/:action", function (match) {
+  document.getElementById("app").innerHTML = `<pre>${JSON.stringify(
+    match,
+    null,
+    2
+  )}</pre>`;
+});
